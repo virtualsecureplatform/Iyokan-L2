425e892d534891ca3c5d1df595536c19fa4985c0
Naoki Matsumoto <m.naoki9911@gmail.com>
2019-10-27 00:49:36 +0900

    Apply clang-format

--- Logic.hpp
+++ Logic.hpp
@@ -1,54 +1,56 @@
 #ifndef LOGIC_HPP
 #define LOGIC_HPP
+
 #include <vector>
 #include <queue>
 #include <iostream>
 
-class Logic
-{
-    public:
-        int id;
-        bool executable;
-        bool executed;
-        int res;
-        std::vector<Logic *> input{};
-        std::vector<Logic *> output{};
-        int InputCount;
-        int ReadyInputCount;
+class Logic {
+public:
+    int id;
+    bool executable;
+    bool executed;
+    int res;
+    std::vector<Logic *> input{};
+    std::vector<Logic *> output{};
+    int InputCount;
+    int ReadyInputCount;
 
-        Logic(int _id){
-            id = _id;
+    Logic(int _id) {
+        id = _id;
+    }
+
+    void PrepareExecution() {
+        InputCount = input.size();
+        ReadyInputCount = 0;
+        std::cout << "Logic" << id << " InputCount:" << InputCount << std::endl;
+
+        if (input.size() == 0) {
+            res = id;
+            executable = true;
+        } else {
+            executable = false;
         }
+        executed = false;
+    }
 
-        void PrepareExecution(){
-            InputCount = input.size();
-            ReadyInputCount = 0;
-            std::cout <<"Logic"<< id <<" InputCount:" << InputCount << std::endl;
-
-            if(input.size() == 0){
-                res = id;
-                executable = true;
-            }else{
-                executable = false;
-            }
-            executed = false;
-        }
-
-        void Execute(std::queue<Logic *> *ReadyQueue){
-            if(!executed){
-                std::cout<< "Executed:Logic" << id << std::endl;
-                for(Logic *logic : output){
-                    logic->ReadyInputCount++;
-                    //std::cout <<"Logic"<< logic->id <<" ReadyInputCount:" << logic->ReadyInputCount << std::endl;
-                    if(logic->ReadyInputCount == logic->InputCount){
-                        logic->executable = true;
-                        //std::cout <<"Executable:Logic"<< logic->id << std::endl;
-                        ReadyQueue->push(logic);
-                    }
+    void Execute(std::queue<Logic *> *ReadyQueue) {
+        if (!executed) {
+            std::cout << "Executed:Logic" << id << std::endl;
+            for (Logic *logic : output) {
+                logic->ReadyInputCount++;
+                //std::cout <<"Logic"<< logic->id <<" ReadyInputCount:" << logic->ReadyInputCount << std::endl;
+                if (logic->ReadyInputCount == logic->InputCount) {
+                    logic->executable = true;
+                    //std::cout <<"Executable:Logic"<< logic->id << std::endl;
+                    ReadyQueue->push(logic);
                 }
-                executed = true;
             }
+            executed = true;
         }
-    private:
+    }
+
+private:
 };
+
 #endif
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <vector>
 #include "Logic.hpp"
 
-int main(){
+int main() {
     Logic logic0(0), logic1(1), logic2(2);
-    std::vector<Logic *> Logics;
-    std::queue<Logic *> ReadyQueue;
+    std::vector < Logic * > Logics;
+    std::queue < Logic * > ReadyQueue;
 
     Logics.push_back(&logic0);
     Logics.push_back(&logic1);
@@ -17,13 +17,13 @@ int main(){
     logic2.input.push_back(&logic0);
     logic2.input.push_back(&logic1);
 
-    for(Logic *logic : Logics){
+    for (Logic *logic : Logics) {
         logic->PrepareExecution();
-        if(logic->executable){
+        if (logic->executable) {
             ReadyQueue.push(logic);
         }
     }
-    while(ReadyQueue.size() > 0){
+    while (ReadyQueue.size() > 0) {
         ReadyQueue.front()->Execute(&ReadyQueue);
         ReadyQueue.pop();
         //std::cout << "ReadQueueSize: " << ReadyQueue.size() << std::endl;
