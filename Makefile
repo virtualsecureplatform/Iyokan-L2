2a4e78ce1e2f5d70d034238b01514f3dbb1203d0
Naoki Matsumoto <m.naoki9911@gmail.com>
2019-10-27 00:13:27 +0900

    Testing implementation of Logic Dependency Resolver

---
+++ Makefile
@@ -0,0 +1,4 @@
+main: main.cpp Logic.hpp
+	g++ -o main main.cpp
+clean:
+	rm main
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include "Logic.hpp"
+
+int main(){
+    Logic logic0(0), logic1(1), logic2(2);
+    std::vector<Logic *> Logics;
+    std::queue<Logic *> ReadyQueue;
+
+    Logics.push_back(&logic0);
+    Logics.push_back(&logic1);
+    Logics.push_back(&logic2);
+
+    logic0.output.push_back(&logic2);
+    logic1.output.push_back(&logic2);
+    logic2.input.push_back(&logic0);
+    logic2.input.push_back(&logic1);
+
+    for(Logic *logic : Logics){
+        logic->PrepareExecution();
+        if(logic->executable){
+            ReadyQueue.push(logic);
+        }
+    }
+    while(ReadyQueue.size() > 0){
+        ReadyQueue.front()->Execute(&ReadyQueue);
+        ReadyQueue.pop();
+        //std::cout << "ReadQueueSize: " << ReadyQueue.size() << std::endl;
+    }
+}
