1438b9f2fbc148fccf84005b38fd4d451994233d
Naoki Matsumoto <m.naoki9911@gmail.com>
2019-12-15 21:40:55 +0900

    fix stream_id

--- cufhe/src/ExecManager.cpp
+++ cufhe/src/ExecManager.cpp
@@ -6,7 +6,7 @@ ExecManager::ExecManager(int num, int _step, bool v, bool isCipher) {
     verbose = v;
     cipher = isCipher;
     for(int i=0;i<10;i++){
-        cores.push_back(new SMCore(0, &readyQueue, isCipher));
+        cores.push_back(new SMCore(i, &readyQueue, isCipher));
     }
 }
 
